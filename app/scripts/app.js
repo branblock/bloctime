8a708147b819405523e97ca0532025ce00e84420
Brandon Block <branblock@gmail.com>
2016-03-09 16:40:15 -0800

    checkpoint 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,37 +16,68 @@ angular.module('bloctime', ['firebase', 'ui.router'])
   });
 })
 
+.constant('SESSION_TIME', {
+  work: 1500,
+  break: 300
+})
+
 .controller('HomeCtrl', ['$scope', '$interval', '$firebaseObject',
   function($scope, $interval, $firebaseObject) {
     var ref = new Firebase('https://brilliant-inferno-3345.firebaseio.com/');
     $scope.data = $firebaseObject(ref);
+
+
+  $scope.isDisabled = false;
+
+  $scope.disableButton = function() {
+    $scope.isDisabled = true;
+  }
 }])
 
-.directive('timer', function($interval) {
+.directive('timer', ['SESSION_TIME', '$interval', function(SESSION_TIME, $interval) {
   return {
     restrict: 'E',
     templateUrl: '/templates/timer.html',
     replace: true,
     scope: { },
     link: function(scope, element, attributes) {
-      scope.counter = 1500;
-      scope.button = 'Start';
-      scope.time = null;
-      scope.startTime = function() {
-        if (scope.button == "Reset") {
-          scope.counter = 1500;
-          scope.button = 'Start';
-          $interval.cancel(time);
+      scope.time = SESSION_TIME.work;
+      scope.workButton = 'Pomodoro!';
+      scope.onBreak = false;
+      var timer;
+
+      scope.countdown = function() {
+        if (scope.workButton === 'Reset Pomodoro') {
+          scope.time = SESSION_TIME.work;
+          $interval.cancel(timer);
+          scope.workButton = 'Pomodoro!';
         } else {
-          time = $interval(function() {
-            scope.counter--;
-            scope.button = 'Reset';
+          scope.workButton = 'Reset Pomodoro';
+          timer = $interval(function() {
+            scope.time--;
+            if (scope.time < 0) {
+              $interval.cancel(timer);
+              if (!scope.onBreak) {
+                scope.onBreak = true;
+                scope.time = SESSION_TIME.break;
+                scope.breakText = 'Break time! Chill.';
+                timer = $interval(function() {
+                  scope.time--;
+                  if (scope.time < 0) {
+                    $interval.cancel(timer);
+                    scope.onBreak = false;
+                    scope.time = SESSION_TIME.work;
+                    scope.workButton = 'Pomodoro!';
+                  }
+                }, 1000);
+              }
+            }
           }, 1000);
         }
- 		  }
+      }
     }
   }
-})
+}])
 
 .filter('timeDisplay', function() {
   return function(seconds) {
